c0a937c7e15c6590f722e3e90d5efcf84598369f
Casper_Desktop <css_mn@yahoo.ie>
2014-01-06 22:06:55 +0100

    added base of QLearning

---
+++ your bot/QLearning.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YourBot {
+    class QLearning {
+        QValueSet store;
+        Random random;
+
+        public QLearning() {
+            this.store = new QValueSet();
+            this.random = new Random();
+        }
+
+        public Action GetAction(State state, Problem problem, float alpha, float gamma, float rho) {
+
+            List<Action> actions = problem.GetAvalaibleActions(state);
+
+            if (this.random.NextDouble() < rho)
+                return PickRandomAction(actions);
+            else
+                return this.store.GetBestAction(state);
+        }
+
+        public Action PickRandomAction(List<Action> actions) {
+            if (actions.Count > 0)
+                return actions[this.random.Next(actions.Count)];
+
+            return null;
+        }
+    }
+
+    class Problem {
+
+        public List<Action> GetAvalaibleActions(State s) {
+            throw new NotImplementedException();
+        }
+    }
+
+    class QValueSet {
+
+        public float this[State s, Action a] {
+            get {
+                throw new NotImplementedException();
+            }
+            set {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        public Action GetBestAction(State s) {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Action {
+
+    }
+
+    class State {
+
+    }
+}
