f6946ae3edf38b91dc814ec7f6221eebca38dbd6
Casper_Desktop <css_mn@yahoo.ie>
2014-01-08 22:47:26 +0100

    QLearningHelper toegevoegd

---
+++ QLearningHelper/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace QLearningHelper {
+    class Program {
+        static void Main(string[] args) {
+
+#if DEBUG
+            System.Diagnostics.Debugger.Launch();
+            while (!System.Diagnostics.Debugger.IsAttached) { }
+#endif
+
+            string learnFile = args[0];
+            string lastStateFile = args[1];
+            string gamelog = args[2];
+            
+
+            float alpha = 0.3f;
+            float gamma = 0.1f;
+
+            int reward = GetReward(gamelog);
+
+
+            QLearning learn = new QLearning();
+            learn.LoadFile(learnFile);
+
+
+            FileStream fs = new FileStream(lastStateFile, FileMode.Open);
+            BinaryReader br = new BinaryReader(fs);
+
+            while (fs.Position < fs.Length) {
+                State oldState = new State((uint)br.ReadInt32());
+                Action action = (Action)br.ReadByte();
+                State newState = new State((uint)br.ReadInt32());
+
+                learn.ProcessReward(reward, oldState, newState, action, alpha, gamma);
+            }
+
+            br.Close();
+            fs.Close();
+
+            learn.SaveFile(learnFile);
+        }
+
+        static int GetReward(string gamelog) {
+
+            FileStream fs = new FileStream(gamelog, FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+
+            string s = sr.ReadToEnd();
+
+            int index = s.IndexOf("\"score\": ") + 10;
+            int end = s.IndexOf(']', index);
+
+            string scoreString = s.Substring(index, end - index);
+
+            string[] scoreStrings = scoreString.Split();
+            scoreStrings[0] = scoreStrings[0].Replace(",", String.Empty);
+
+            sr.Close();
+            fs.Close();
+
+            return int.Parse(scoreStrings[0]) - int.Parse(scoreStrings[1]);
+        }
+
+
+        /*public void ProcessScores(IGameState state, int score1, int score2) {
+            int reward = score1 - score2;
+
+            for (int col = 0; col < state.Width; ++col) {
+                for (int row = 0; row < state.Height; ++row) {
+                    Location location = new Location(row, col);
+                    byte position = location.ToByte();
+
+                    if (this.doneMoves[position] != null) {
+                        State newState = GetState(state, location);
+
+                        foreach (StateAction sa in this.doneMoves[position]) {
+                            this.learn.ProcessReward(-0.1f, sa.State, newState, sa.Action, this.alpha, this.gamma);
+                        }
+                    }
+                }
+            }
+        }*/
+    }
+}
---
+++ QLearningHelper/QLearning.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.IO;
+
+namespace QLearningHelper {
+    public enum Action { North = 0, South = 1, East = 2, West = 3, None = 4 };
+
+    class QLearning {
+        QValueSet store;
+        Random random;
+
+
+
+        public QLearning() {
+            this.store = new QValueSet();
+            this.random = new Random();
+        }
+
+        public Action GetAction(State state, float rho) {
+
+            //List<Action> actions = problem.GetAvalaibleActions(state);
+            List<Action> actions = new List<Action> { Action.North, Action.East, Action.South, Action.West, Action.None };
+
+            if (this.random.NextDouble() < rho)
+                return PickRandomAction(actions);
+            else
+                return this.store.GetBestAction(state);
+        }
+
+
+        public void ProcessReward(float reward, State oldState, State newState, Action action, float alpha, float gamma) {
+
+            float Q = store[oldState, action];
+            float maxQ = store[newState, store.GetBestAction(newState)];
+
+            Q = (1 - alpha) * Q + alpha * (reward + gamma * maxQ);
+
+            store[oldState, action] = Q;
+        }
+
+
+        private Action PickRandomAction(List<Action> actions) {
+            if (actions.Count > 0)
+                return actions[this.random.Next(actions.Count)];
+
+            return Action.None;
+        }
+
+
+        public void LoadFile(string s) {
+            FileStream fs = File.Open(s, FileMode.Open);
+            BinaryReader br = new BinaryReader(fs);
+
+
+            while (fs.Position < fs.Length) {
+                State state = new State(br.ReadUInt32());
+
+                QSetItem newItem = new QSetItem();
+                newItem[Action.North] = br.ReadSingle();
+                newItem[Action.South] = br.ReadSingle();
+                newItem[Action.East] = br.ReadSingle();
+                newItem[Action.West] = br.ReadSingle();
+                newItem[Action.None] = br.ReadSingle();
+
+                this.store.Add(state, newItem);
+            }
+        }
+
+
+        public void SaveFile(string s) {
+            FileStream fs = File.Open(s, FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs);
+
+            Hashtable set = this.store.set;
+
+            foreach (State state in set.Keys) {
+                QSetItem item = (QSetItem)set[state];
+                
+                bw.Write(state.Value);
+
+                bw.Write(item[Action.North]);
+                bw.Write(item[Action.South]);
+                bw.Write(item[Action.East]);
+                bw.Write(item[Action.West]);
+                bw.Write(item[Action.None]);
+            }
+
+            bw.Close();
+            fs.Close();
+        }
+    }
+
+
+    struct StateAction {
+        public State State { get; set; }
+        public Action Action { get; set; }
+    }
+
+
+    class Problem {
+        public List<Action> GetAvalaibleActions(State s) {
+            throw new NotImplementedException();
+        }
+    }
+
+    class QValueSet {
+
+        //dont want to make this public but is better for saving the file...
+        public Hashtable set;
+
+        Random random;
+
+        public QValueSet() {
+            this.set = new Hashtable();
+            this.random = new Random();
+        }
+
+        public float this[State s, Action a] {
+            get {
+                if (this.set.ContainsKey(s))
+                    return ((QSetItem)this.set[s])[a];
+                return 0;
+            }
+            set {
+                if (this.set.ContainsKey(s)) {
+                    ((QSetItem)this.set[s])[a] = value;
+                } else {
+                    QSetItem newItem = new QSetItem();
+                    newItem[a] = value;
+                    this.set.Add(s, newItem);
+                }
+
+            }
+        }
+
+
+        public void Add(State s, QSetItem item) {
+            this.set.Add(s, item);
+        }
+
+
+        public Action GetBestAction(State s) {
+            float maxQ = float.MinValue;
+            Action result = default(Action);
+
+            if (this.set.ContainsKey(s)) {
+                foreach (Action a in Enum.GetValues(typeof(Action))) {
+                    float Q = ((QSetItem)this.set[s])[a];
+                    if (Q > maxQ) {
+                        maxQ = Q;
+                        result = a;
+                    }
+                }
+            } else {
+                result = (Action)random.Next(5);
+            }
+
+            return result;
+        }
+    }
+
+    class QSetItem {
+        private float[] values;
+
+        public QSetItem() {
+            this.values = new float[5]; //5 actions
+        }
+
+        public float this[Action a] {
+            get {
+                return this.values[(int)a];
+            }
+            set {
+                this.values[(int)a] = value;
+            }
+        }
+    }
+
+
+    public enum QTile { Ant, Enemy, Food, None };
+
+
+    //1 2 3
+    //4   5
+    //6 7 8
+    struct State {
+        public uint Value { get; private set; }
+
+        public State(QTile[] tiles, byte position)
+            : this() {
+            
+            this.Value = 0;
+
+            for (int i = 0; i < 8; ++i) {
+                this.Value |= ((uint)tiles[i] << (2 * i));
+            }
+
+            this.Value |= (uint)position << 24;
+        }
+
+        public State(uint key)
+            : this() {
+            
+            this.Value = key;
+        }
+    }
+}
