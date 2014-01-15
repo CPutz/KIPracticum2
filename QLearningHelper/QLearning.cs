171cb19e12388175519aadfae096ea0d88db3e0f
Casper_Desktop <css_mn@yahoo.ie>
2014-01-15 21:13:17 +0100

    updated QLearning file in QLearningHelper

--- QLearningHelper/QLearning.cs
+++ QLearningHelper/QLearning.cs
@@ -11,15 +11,21 @@ namespace QLearningHelper {
         Random random;
 
 
-
         public QLearning() {
             this.store = new QValueSet();
             this.random = new Random();
         }
 
+
+        /// <summary>
+        /// Gets an action that is possible in a particular state.
+        /// </summary>
+        /// <param name="state">The state to choose an action in.</param>
+        /// <param name="rho">The probability of choosing a random action.</param>
+        /// <returns>the best possible action in State <paramref name="state"/> with probability 
+        /// <paramref name="rho"/> to return a random action.</returns>
         public Action GetAction(State state, float rho) {
 
-            //List<Action> actions = problem.GetAvalaibleActions(state);
             List<Action> actions = new List<Action> { Action.North, Action.East, Action.South, Action.West, Action.None };
 
             if (this.random.NextDouble() < rho)
@@ -29,6 +35,15 @@ namespace QLearningHelper {
         }
 
 
+        /// <summary>
+        /// Porcesses a given reward.
+        /// </summary>
+        /// <param name="reward">The reward that was earned by taking the action.</param>
+        /// <param name="oldState">The state before the <paramref name="action"/> was taken.</param>
+        /// <param name="newState">The state we ended up in after taking the <paramref name="action"/>.</param>
+        /// <param name="action">The action that was taken.</param>
+        /// <param name="alpha">The Learning rate.</param>
+        /// <param name="gamma">The Discount rate.</param>
         public void ProcessReward(float reward, State oldState, State newState, Action action, float alpha, float gamma) {
 
             float Q = store[oldState, action];
@@ -40,6 +55,11 @@ namespace QLearningHelper {
         }
 
 
+        /// <summary>
+        /// Picks a random action from a list of acitons.
+        /// </summary>
+        /// <param name="actions">The list to choose an actions from.</param>
+        /// <returns>An action from the list <paramref name="actions"/>.</returns>
         private Action PickRandomAction(List<Action> actions) {
             if (actions.Count > 0)
                 return actions[this.random.Next(actions.Count)];
@@ -48,8 +68,18 @@ namespace QLearningHelper {
         }
 
 
-        public void LoadFile(string s) {
-            FileStream fs = File.Open(s, FileMode.Open);
+        //file format:
+        //1 int (state)
+        //5 floats (5 actions)
+
+        /// <summary>
+        /// Loads a Q-learning file containing Q-values.
+        /// </summary>
+        /// <param name="filename">The name of the Q-learning file.</param>
+        public void LoadFile(string filename) {
+
+
+            FileStream fs = File.Open(filename, FileMode.OpenOrCreate);
             BinaryReader br = new BinaryReader(fs);
 
 
@@ -71,8 +101,12 @@ namespace QLearningHelper {
         }
 
 
-        public void SaveFile(string s) {
-            FileStream fs = File.Open(s, FileMode.Create);
+        /// <summary>
+        /// Saves the Q-values stored in store in a file.
+        /// </summary>
+        /// <param name="filename">The name of the Q-learning file.</param>
+        public void SaveFile(string filename) {
+            FileStream fs = File.Open(filename, FileMode.Create);
             BinaryWriter bw = new BinaryWriter(fs);
 
             Hashtable set = this.store.set;
@@ -95,18 +129,15 @@ namespace QLearningHelper {
     }
 
 
-    struct StateAction {
+    class StateAction {
         public State State { get; set; }
         public Action Action { get; set; }
     }
 
 
-    class Problem {
-        public List<Action> GetAvalaibleActions(State s) {
-            throw new NotImplementedException();
-        }
-    }
-
+    /// <summary>
+    /// A set that contains Q-values.
+    /// </summary>
     class QValueSet {
 
         //dont want to make this public but is better for saving the file...
@@ -143,18 +174,30 @@ namespace QLearningHelper {
         }
 
 
+        /// <summary>
+        /// Gets the best possible action in a certain state.
+        /// </summary>
+        /// <param name="s">The state to choose an action in.</param>
+        /// <returns>Returns the action with the highest Q-value. If all values are zero then
+        /// a random value will be returned.</returns>
         public Action GetBestAction(State s) {
             float maxQ = float.MinValue;
             Action result = default(Action);
+            List<Action> actions = new List<Action>();
 
             if (this.set.ContainsKey(s)) {
                 foreach (Action a in Enum.GetValues(typeof(Action))) {
                     float Q = ((QSetItem)this.set[s])[a];
                     if (Q > maxQ) {
+                        actions = new List<Action>();
                         maxQ = Q;
-                        result = a;
+                        actions.Add(a);
+                    } else if (Q == maxQ) {
+                        actions.Add(a);
                     }
                 }
+
+                result = actions[random.Next(actions.Count)];
             } else {
                 result = (Action)random.Next(5);
             }
@@ -162,6 +205,7 @@ namespace QLearningHelper {
             return result;
         }
     }
+
 
     class QSetItem {
         private float[] values;
@@ -181,12 +225,17 @@ namespace QLearningHelper {
     }
 
 
+    // The information stored about each tile in a state
     public enum QTile { Ant, Enemy, Food, None };
 
+    //The tiles that are stored in a state where x is an ant.
+    //      9
+    //    1 2 3
+    // 12 4 x 5 10
+    //    6 7 8
+    //      11
+    
 
-    //1 2 3
-    //4   5
-    //6 7 8
     struct State {
         public uint Value { get; private set; }
 
@@ -195,17 +244,25 @@ namespace QLearningHelper {
             
             this.Value = 0;
 
+            //The last 24 bits are used to store 12 2-bit values representing
+            //the QTile value in each of the 12 tiles around the position.
             for (int i = 0; i < tiles.Length; ++i) {
-                this.Value |= ((uint)tiles[i] << (2 * i));
+                this.Value |= ((uint)tiles[i] << (2 * i + 8));
             }
 
-            this.Value |= (uint)position << 24;
+            //The first 8 bits are used to store a position value (between 0 and 143)
+            this.Value |= (uint)position;
         }
 
         public State(uint key)
             : this() {
             
             this.Value = key;
+        }
+
+        public byte GetPosition() {
+            //                                00000000 00000000 00000000 11111111
+            return (byte)(this.Value & 255);
         }
     }
 }
